0e5f4b004ad5c9b58c05861c69880a2da1c44db4
Suman Adhikari <expertxkid@gmail.com>
2025-04-21 10:14:14 -0500

    init: init project

---
+++ src/test/java/edu/miu/cs489/exam/galactix/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs489.exam.galactix.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
